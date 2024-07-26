3d431cd528f49cd8788ebc483c60001c71a8a9f1
Sevval Sayın <ssayin0498@gmail.com>
2024-07-26 18:02:37 +0300

    deneme hata

--- src/pages/auth/index.js
+++ src/pages/auth/index.js
@@ -1,4 +1,5 @@
 // src/pages/auth/index.js
 export { default as SignIn } from './sign-in';
 export { default as SignUp } from './sign-up';
-export {default as SignUpdate} from './sign-update'
+export {default as SignUpdate} from './sign-update';
+export {default as Register} from './Register';
