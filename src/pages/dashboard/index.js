6dd046ba1930d9a960ecd4323f7b0258c8f103d1
Sevval Sayın <ssayin0498@gmail.com>
2024-07-26 15:34:07 +0300

    büyük harf

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,9 +1,9 @@
 // src/pages/dashboard/index.js
 export { default as Home } from "./home";
-export { default as Assay } from "./assay";
+export { default as Assay } from "./Assay";
 export { default as Profile } from "./profile";
-export { default as Prescriptions } from "./prescriptions";
-export { default as Radiological } from "./radiological";
-export { default as VaccineAllergy } from "./vaccineAllergy";
-export { default as Results } from "./results";
-export { default as Hospital } from "./hospital";
+export { default as Prescriptions } from "./Prescriptions";
+export { default as Radiological } from "./Radiological";
+export { default as VaccineAllergy } from "./VaccineAllergy";
+export { default as Results } from "./Results";
+export { default as Hospital } from "./Hospital";
