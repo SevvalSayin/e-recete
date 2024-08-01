2ab61722544afa68e469b9df759d2df3a5619a59
Sevval Sayın <ssayin0498@gmail.com>
2024-08-01 09:20:41 +0300

    cors hatası deneme

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -22,6 +22,7 @@ export const insertDocument = async (document) => {
   };
 
   try {
+    console.log('Making request to insert document...');
     const response = await axiosInstance.post('/action/insertOne', data);
     console.log('Response Status Code:', response.status);
     console.log('Parsed Result:', response.data);
@@ -29,15 +30,12 @@ export const insertDocument = async (document) => {
   } catch (error) {
     console.error('API request error:', error.message);
     if (error.response) {
-    
       console.error('Error Data:', error.response.data);
       throw new Error(`Error: ${error.response.status} - ${error.response.data.message || error.response.statusText}`);
     } else if (error.request) {
-     
       console.error('No response received:', error.request);
       throw new Error('No response received from the server. Check your network connection or CORS policy.');
     } else {
-     
       console.error('Error setting up request:', error.message);
       throw new Error('Error setting up the request.');
     }
