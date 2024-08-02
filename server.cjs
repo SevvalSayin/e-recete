3acf73b3ada176bd03fd850a6121b2e5b5bc6625
Sevval Sayın <ssayin0498@gmail.com>
2024-08-02 15:46:07 +0300

    hata deneme3

--- server.cjs
+++ server.cjs
@@ -2,27 +2,43 @@ const express = require('express');
 const serveStatic = require('serve-static');
 const path = require('path');
 const cors = require('cors');
+const request = require('request');
 
 const app = express();
 
 const corsOptions = {
-  origin: 'https://e-recete-f15179303064.herokuapp.com', // Allow only your Heroku app
+  origin: 'http://localhost:5173', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'api-key', 'Accept']
 };
 
 app.use(cors(corsOptions));
-app.use(express.json()); // To parse JSON bodies
+app.use(express.json()); 
 
-app.use(serveStatic(path.join(__dirname, 'dist')));
 
-// Example endpoint
-app.post('/api/action/find', async (req, res) => {
-  // Handle the find action
-  const { filter } = req.body;
-  // Your database logic here
-  res.json({ message: 'Find action received', filter });
+app.use('/api', (req, res) => {
+  const url = `https://eu-central-1.aws.data.mongodb-api.com/app/data-nauitwn/endpoint/data/v1${req.url}`;
+  const options = {
+    url: url,
+    headers: {
+      'api-key': 'Rvc6CNklg8YuyDRi014MSZennyqBH5Xib8yhWMSDJ4kk42HOnozkB0T5IVw1C9TG' 
+    },
+    method: req.method,
+    json: true,
+    body: req.body
+  };
+  request(options, (error, response, body) => {
+    if (error) {
+      console.error('Proxy error:', error);
+      res.status(500).send(error);
+    } else {
+      res.status(response.statusCode).json(body);
+    }
+  });
 });
+
+// Serve static files
+app.use(serveStatic(path.join(__dirname, 'dist')));
 
 // Handle all routes by serving 'index.html'
 app.get('*', (req, res) => {
