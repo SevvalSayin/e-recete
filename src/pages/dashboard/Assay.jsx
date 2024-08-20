f7eb6cc6c2f73d535ca6b9f2bf4fefc1bfd0922b
Şevval Sayın | Ante Holding <sevval.sayin@anteholding.com>
2024-08-20 14:50:13 +0300

    eski commite geri döndüm canlı denemeye alıyorum deneme1

--- src/pages/dashboard/Assay.jsx
+++ src/pages/dashboard/Assay.jsx
@@ -14,27 +14,35 @@ const atcFieldMapping = {
 };
 
 const icdFieldMapping = {
-  __EMPTY: 'Name',
-  __EMPTY_1: 'Code',
-  __EMPTY_2: 'Upper Code',
-  __EMPTY_3: 'Level',
-  __EMPTY_4: 'High Risk',
-  __EMPTY_5: 'Pregnancy Status'
+  'Adı': 'Name',
+  'Kodu': 'Code',
+  'Üst Kodu': 'Upper Code',
+  'Seviye': 'Level',
+  'Yüksek Riskli Gebelik': 'High Risk'
 };
 
+
+
+
+
+// Updated mapFields function
 function mapFields(data, fieldMapping) {
   return data.map(row => {
     const newRow = {};
+    let isEmpty = true; // Track if row is empty
+
     Object.keys(row).forEach(key => {
       const mappedKey = fieldMapping[key];
-      if (mappedKey) {
+      if (mappedKey && row[key] !== undefined && row[key] !== null && row[key] !== '') {
         newRow[mappedKey] = row[key];
+        isEmpty = false; // Set to false if any valid data is found
       }
     });
-    return newRow;
-  });
-}
 
+    // Only return rows that have mapped data
+    return isEmpty ? null : newRow;
+  }).filter(row => row !== null); // Remove empty rows
+}
 async function fetchAndProcessFile(fileUrl, isAtc = false) {
   try {
     console.log('Fetching file from URL:', fileUrl);
@@ -50,25 +58,34 @@ async function fetchAndProcessFile(fileUrl, isAtc = false) {
     const worksheet = workbook.Sheets[sheetName];
     const data = XLSX.utils.sheet_to_json(worksheet);
 
-    console.log('Raw Excel Data:', data);
+    // Log the first few rows of raw data and their keys
+    if (data.length > 0) {
+      console.log('Raw Excel Data (First 5 Rows):', data.slice(0, 5)); // Log first 5 rows of raw data
+      console.log('Keys in the First Row:', Object.keys(data[0]));     // Log the keys in the first row
+    } else {
+      console.error('Excel data is empty.');
+      return;
+    }
 
+    // Map the data using appropriate field mapping (ATC or ICD)
     const mappedData = isAtc ? mapFields(data, atcFieldMapping) : mapFields(data, icdFieldMapping);
 
     if (Array.isArray(mappedData) && mappedData.length > 0) {
-      console.log('Mapped Data:', mappedData); // Mapped verileri kontrol edin
+      console.log('Mapped Data:', mappedData);
       if (isAtc) {
         await insertManyDocuments(mappedData);
       } else {
         await sendRawDataToApiService(mappedData);
       }
     } else {
-      console.error('Mapped data is not an array or is empty:', mappedData);
+      console.error('Mapped data is empty or invalid:', mappedData);
     }
 
   } catch (error) {
     console.error('Error fetching or processing file:', error.message);
   }
 }
+
 
 function Assay() {
   const handleATCClick = () => {
